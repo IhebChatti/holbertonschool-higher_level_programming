251e412ac6186aa1169425b598bc0ab0e6a9c75a
IhebChatti <iheb.chatti@holbertonschool.com>
2020-04-28 04:40:43 +0100

    toUPPERCASE

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        c = ord(i)
+        if ord(i) >= ord('a') and ord(i) <= ord('z'):
+            c = ord(i) - 32
+        print("{}".format(chr(c)), end='')
+    print("")

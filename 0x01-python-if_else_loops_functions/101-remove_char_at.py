22905fe6dde8c81643a5de3260fb897a2de5b5b9
IhebChatti <iheb.chatti@holbertonschool.com>
2020-04-28 16:40:14 +0100

    pop char

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     str_cpy = str
-    if len(str_cpy) > n:
+    if len(str_cpy) >= n:
         str_cpy = str_cpy[0:n:] + str_cpy[n+1::]
     return(str_cpy)

109379078b1b5af8ef81fa4414193bad67ae9c48
IhebChatti <iheb.chatti@holbertonschool.com>
2020-04-30 17:07:53 +0100

    hidden discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import hidden_4
-_list = dir(hidden_4)
-for i in range(len(_list)):
-    if _list[i][0] != "_":
-        print('{}'.format(_list[i]))
+if __name__ == "__main__":
+    import hidden_4
+    _list = dir(hidden_4)
+    for i in range(len(_list)):
+        if _list[i][0] != "_":
+            print('{}'.format(_list[i]))

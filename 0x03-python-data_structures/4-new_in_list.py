bb4dd4bb6dbe2822fbec81ffba21acd977a321d7
IhebChatti <iheb.chatti@holbertonschool.com>
2020-05-05 18:44:19 +0100

    new in list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,9 +2,9 @@
 def new_in_list(my_list, idx, element):
     my_list_cpy = []
     if (idx < 0):
-        return (my_list_cpy)
+        return (my_list)
     elif (idx > len(my_list) - 1):
-        return (my_list_cpy)
+        return (my_list)
     else:
         for i in range(len(my_list)):
             my_list_cpy.append(my_list[i])

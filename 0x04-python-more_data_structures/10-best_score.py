8db6d244b943fb8bed5146fb0a38623b3ea8d270
IhebChatti <iheb.chatti@holbertonschool.com>
2020-05-07 18:31:37 +0100

    max dict

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary:
-        return max(a_dictionary)
-    else:
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
+    return max(a_dictionary)

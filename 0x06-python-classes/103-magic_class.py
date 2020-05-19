e0bd0331976e10f490c5a74d5cda68a05afd9e70
IhebChatti <iheb.chatti@holbertonschool.com>
2020-05-19 17:42:10 +0100

    fixing magic line numbers

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+""" defining magic class """
+
+
 import math
 class MagicClass:
+    """ Magic Class """
     def __init__(self, radius=0):
+        """ defining init
+        args:
+            radius """
         self.__radius = 0
         if type(radius) is not int or type(radius) is not float:
             raise TypeError('radius must be a number')

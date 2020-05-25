b5d47edcceab450c1b4bb84d36485ff2b9828edb
IhebChatti <iheb.chatti@holbertonschool.com>
2020-05-25 23:09:59 +0100

    rectange..

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -117,8 +117,8 @@ class Rectangle:
     def __del__(self):
         """prints a message when instance is deleted
         """
-        print('Bye rectangle...')
         Rectangle.number_of_instances -= 1
+        print('Bye rectangle...')
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):

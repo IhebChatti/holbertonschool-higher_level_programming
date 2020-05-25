54c89d3153eecc23d7026b1732eb45b47bf558d1
IhebChatti <iheb.chatti@holbertonschool.com>
2020-05-25 21:59:19 +0100

    added static mehtod

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -141,8 +141,7 @@ class Rectangle:
             raise TypeError('rect_2 must be an instance of Rectangle')
         if rect_1.area() < rect_2.area():
             return rect_2
-        else:
-            return rect_1
+        return rect_1
 
     @classmethod
     def square(cls, size=0):

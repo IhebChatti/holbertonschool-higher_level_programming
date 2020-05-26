7edebddcb37cb8f775307285479f39d789b7e50c
IhebChatti <iheb.chatti@holbertonschool.com>
2020-05-26 22:00:38 +0100

    pep8 fix

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 def copy_list(l):
-    """copy_list
-
-    Arguments:
-        l {[list]} -- [list to be copied]
-
-    Returns:
-        [list] -- [the copy of the list l]
-    """
     return list(l)

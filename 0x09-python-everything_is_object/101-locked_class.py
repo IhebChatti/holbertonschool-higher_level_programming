ac165234703fe187f244f1041b72a1ead404c8d1
IhebChatti <iheb.chatti@holbertonschool.com>
2020-05-26 21:58:16 +0100

    added docstring

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""Defining a locked class
+"""
+
+
 class LockedClass:
-    """Locked class
+    """Locked class that prevents
+    the user from dynamically creating
+    new instance attributes,
     """
-    __slots__ = "first_name"
+    __slots__ = ("first_name")

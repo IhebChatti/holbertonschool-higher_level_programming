ffa138946b39c87bebe4b05de11866712efe2598
IhebChatti <iheb.chatti@holbertonschool.com>
2020-06-02 15:51:52 +0100

    basegeometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry definition
+    """
+
+    def area(self):
+        """area not implemented
+
+        Raises:
+            Exception: [area is not implemented yet]
+        """
+        raise Exception('area() is not implemented')

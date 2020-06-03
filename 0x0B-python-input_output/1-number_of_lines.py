e8216700563fbbc208292bcd00853f917540d091
IhebChatti <iheb.chatti@holbertonschool.com>
2020-06-03 12:32:39 +0100

    number of lines

---
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""number of lines function
+"""
+
+
+def number_of_lines(filename=""):
+    """number_of_lines
+
+    Keyword Arguments:
+        filename {str} -- [the file to get it's nmbr of lines] (default: {""})
+
+    Returns:
+        [int] -- [number of lines]
+    """
+    count = 0
+    with open("my_file_0.txt",  encoding="utf-8") as myFile:
+        for line in myFile:
+            count += 1
+    return count

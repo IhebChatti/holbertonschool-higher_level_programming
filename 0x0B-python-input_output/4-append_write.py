374b32101f7b9a157d873ce670e6f08faceaa6fc
IhebChatti <iheb.chatti@holbertonschool.com>
2020-06-03 13:10:01 +0100

    append

---
+++ 0x0B-python-input_output/4-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""[append write file]
+"""
+
+
+def append_write(filename="", text=""):
+    """append_write
+
+    Keyword Arguments:
+        filename {str} -- [the name of file] (default: {""})
+        text {str} -- [the text to be appended] (default: {""})
+    """
+    with open(filename, mode="a", encoding="utf-8") as myFile:
+        return(myFile.write(text))

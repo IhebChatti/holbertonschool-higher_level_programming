babdd830e4802c8added2f853c720934371742b4
IhebChatti <iheb.chatti@holbertonschool.com>
2020-06-03 19:03:11 +0100

    docstring added

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
+"""[append_after function]
+"""
 
 
 def append_after(filename="", search_string="", new_string=""):
+    """[append_after]
+
+    Keyword Arguments:
+        filename {str} -- [name of file] (default: {""})
+        search_string {str} -- [string to be searched] (default: {""})
+        new_string {str} -- [new string] (default: {""})
+    """
     with open(filename, mode="r+", encoding="utf-8") as MyFile:
         txt = ""
         for i in MyFile:

f3019a43d4be8a02a1274d599a5095ba0888b75a
IhebChatti <iheb.chatti@holbertonschool.com>
2020-06-05 15:35:06 +0100

    added docstring

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
+"""[Base Definition]
+"""
 
 
 class Base:
+    """[Base class]
+    """
     __nb_objects = 0
+
     def __init__(self, id=None):
+        """[Base initialization]
+
+        Args:
+            id ([int], optional): [the id]. Defaults to None.
+        """
+
         if id is not None:
             self.id = id
         else:

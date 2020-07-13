e9e91124dc4b16ee17c56f2a98aae1ea19923d7b
IhebChatti <iheb.chatti@holbertonschool.com>
2020-07-13 12:59:54 +0100

    max temp

---
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -0,0 +1,2 @@
+-- a script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending).
+SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state ASC;

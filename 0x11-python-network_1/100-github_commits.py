fa0f4c0f34336f765c196e3275a79b157837d11a
IhebChatti <iheb.chatti@holbertonschool.com>
2020-08-24 17:53:00 +0100

    fixed limit

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,11 +8,11 @@ if __name__ == "__main__":
     repo = argv[1]
     owner = argv[2]
     url = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
-    try:
         req = requests.get(url)
         data = req.json()
+    try:
         for i in range(10):
             print("{}: {}".format(data[i].get("sha"), data[i].get(
                 "commit").get("author").get("name")))
-    except ValueError:
+    except:
         pass

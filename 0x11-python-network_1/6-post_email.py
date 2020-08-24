a583d412dce899d26dde57ca2f7ccb14c64dd367
IhebChatti <iheb.chatti@holbertonschool.com>
2020-08-24 14:49:00 +0100

    post email #1

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""[Python script that takes in a URL and an email,
+    sends a POST request to the passed URL with the email
+    as a parameter, and displays the body of the response ]
+"""
+import requests
+from sys import argv
+if __name__ == "__main__":
+    url = argv[1]
+    values = {"email": argv[2]}
+
+    r = requests.post(url, values)
+    print(r.text)

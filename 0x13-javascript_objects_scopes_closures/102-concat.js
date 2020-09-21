08ae0c1927bebd3e6a714732f3e89993e30c3910
IhebChatti <iheb.chatti@holbertonschool.com>
2020-09-21 19:31:09 +0100

    concat files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,4 +3,4 @@ const fs = require('fs');
 const args = process.argv;
 const a = fs.readFileSync(args[2], 'utf-8');
 const b = fs.readFileSync(args[3], 'utf-8');
-fs.writeFileSync(args[4], a + '\n' + b + '\n');
+fs.writeFileSync(args[4], a + b);

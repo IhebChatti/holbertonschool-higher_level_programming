c165d9091f6d4a46efe78cc63357814f58ea6f28
IhebChatti <iheb.chatti@holbertonschool.com>
2020-09-22 16:14:52 +0100

    write file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const fs = require('fs');
+const fileName = process.argv[2];
+const content = process.argv[3];
+fs.writeFile(fileName, content, 'utf-8', (error) => {
+  if (error) {
+    console.log(error);
+  }
+});

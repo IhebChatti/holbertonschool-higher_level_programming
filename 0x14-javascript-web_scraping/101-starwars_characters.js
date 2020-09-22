b92bbc7de68677e548ac39f0096c11582b9467a6
IhebChatti <iheb.chatti@holbertonschool.com>
2020-09-22 19:59:16 +0100

    finally

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,15 +2,19 @@
 const request = require('request');
 const movieId = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/' + movieId;
-let result = [];
+let chars;
+const ordNames = {};
 request.get(url, (error, respponse, body) => {
-  (error) ? console.log(error) : result = JSON.parse(body).characters;
-  for (const charId in result) {
-    const orderedNames = {};
-    request.get(result[charId], (error, response, body) => {
-      (error) ? console.log(error) : orderedNames[charId] = JSON.parse(body).name;
-      if (Object.keys(result) !== Object.keys(orderedNames)) {
-        console.log(orderedNames[charId]);
+  (error) ? console.log(error) : chars = JSON.parse(body).characters;
+  for (const id of chars) {
+    request.get(id, (error, response, body) => {
+      if (!error) {
+        ordNames[id] = JSON.parse(body).name;
+        if (Object.values(ordNames).length === chars.length) {
+          for (const id of chars) {
+            console.log(ordNames[id]);
+          }
+        }
       }
     });
   }
